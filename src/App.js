3baea10b63944ffecbc4b96db2a3a538afd9ffe2
Bilal Malik <45467181+bilalmalik4321@users.noreply.github.com>
2020-03-28 19:06:34 -0400

    Fixed a merge conflict

--- src/App.js
+++ src/App.js
@@ -1,36 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-<<<<<<< Updated upstream
-import { Button } from 'antd';
-import 'antd/dist/antd.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       <Button type="primary" className="Button" >Sign Up</Button>
-        <h1 className="Title">Stay Safe Keep Space</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
-
-      </header>
-      <header className="App-body">
-       <h1>Listings</h1>
-      </header>
-    </div>
-=======
-import styled from 'styled-components';
-import { Layout } from 'antd';
-import AppFooter from './AppFooter';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import styled from "styled-components";
+import { Layout } from "antd";
+import AppFooter from "./AppFooter";
 
 const { Footer } = Layout;
 
 const AppWrapper = styled(Layout)`
-  background: #FFFFFF;
+  background: #ffffff;
   height: 100%;
 `;
 
@@ -39,9 +17,8 @@ function App() {
     <AppWrapper>
       <NavBar />
       <LandingContent />
-      <AppFooter/>
+      <AppFooter />
     </AppWrapper>
->>>>>>> Stashed changes
   );
 }
 
