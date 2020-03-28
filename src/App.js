a5f677bf9cfebf4c40dadc5769a087dab6f2119a
Bilal Malik <45467181+bilalmalik4321@users.noreply.github.com>
2020-03-28 19:32:19 -0400

    Included necessary imports and semi colons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import "antd/dist/antd.css";
 import "./App.css";
+import logo from "./logo.svg";
+import React, { Component } from "react";
 import styled from "styled-components";
 import { Layout } from "antd";
 import AppFooter from "./AppFooter";
-import "antd/dist/antd.css";
+import NavBar from "./components/NavBar";
+import LandingContent from "./components/LandingContent";
 
 const { Footer } = Layout;
 
