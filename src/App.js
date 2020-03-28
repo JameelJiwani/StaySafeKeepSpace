2c160f6e78d67e0194c11d2a369964dcd172f43f
Bilal Malik <45467181+bilalmalik4321@users.noreply.github.com>
2020-03-28 01:50:11 -0400

    Some changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd';
+import 'antd/dist/antd.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+       <Button type="primary" className="Button" >Sign Up</Button>
+        <h1 className="Title">Stay Safe Keep Space</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+
+      </header>
+      <header className="App-body">
+       <h1>Listings</h1>
       </header>
     </div>
   );
