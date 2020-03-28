e8961d5add33f3ca52e420d94f5089750b7360a2
Jameel <jameeljiwani@gmail.com>
2020-03-28 01:40:11 -0400

    added aa button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import 'antd/dist/antd.css';
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd';
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button type="primary">Primary</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
