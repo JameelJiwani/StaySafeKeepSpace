d07d3816ccfc2fa12a33b16065dad6d4295825a3
Jameel <jameeljiwani@gmail.com>
2020-03-29 05:34:38 -0400

    fixed form validation

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import * as firebase from 'firebase';
+import * as firebase from './firebase';
 
 ReactDOM.render(
   <React.StrictMode>
